b8accad7ea067a391fc40b979a107afee69e9e8b
Maksyutov Vyacheslav <thelastslike@gmail.com>
2021-08-23 11:05:02 +0500

    feat:find the solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+  let minimum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    let a = array[i];
+    if (minimum < a) {
+      minimum = minimum
+    } else {
+      minimum = a
+    }
+  }
+  if (minimum === undefined) { return 0 } else {
+    return minimum;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+  let maximum = 0
+  if (array === undefined || array.length === 0) {
+    return 0
+  } else {
+    array.forEach(a => {
+      maximum < a ? maximum = a : ''
+    });
+  }
+  return maximum
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let a = 0;
+  if (array === undefined || array.length === 0) {
+    return 0
+  }
+  else {
+    for (let i = 0; i < array.length; i++) {
+      a += array[i] / array.length;
+    }
+    return a.toFixed(2);
+  }
 }
